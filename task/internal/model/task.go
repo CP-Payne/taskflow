4de16337b873cd9ef2d9261b5c666ce5ef51e215
CP Payne <charlethical1@gmail.com>
2025-04-08 21:52:39 +0200

    Implement task models

--- task/internal/model/task.go
+++ task/internal/model/task.go
@@ -4,6 +4,9 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	api "github.com/CP-Payne/taskflow/task/internal/proto/api/v1"
 )
 
 type Status int
@@ -16,10 +19,43 @@ const (
 
 type Task struct {
 	ID          uuid.UUID
+	UserID      uuid.UUID
 	Title       string
 	Description string
 	Status      Status
 	AssignedTo  *uuid.UUID
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
+}
+
+func (t *Task) ToProto() *api.Task {
+	var assignedTo *api.UUID
+	if t.AssignedTo != nil {
+		assignedTo = UuidToProtoUUID(*t.AssignedTo)
+	}
+	return &api.Task{
+		Id:          UuidToProtoUUID(t.ID),
+		UserId:      UuidToProtoUUID(t.UserID),
+		Title:       t.Title,
+		Description: t.Description,
+		Status:      api.Status(t.Status),
+		AssignedTo:  assignedTo,
+		CreatedAt:   timestamppb.New(t.CreatedAt),
+		UpdatedAt:   timestamppb.New(t.UpdatedAt),
+	}
+}
+
+func UuidToProtoUUID(id uuid.UUID) *api.UUID {
+	return &api.UUID{
+		Value: id.String(),
+	}
+}
+
+func TaskListToProto(tasks []Task) []*api.Task {
+	taskProtoList := make([]*api.Task, len(tasks))
+	for i, t := range tasks {
+		taskProtoList[i] = t.ToProto()
+	}
+
+	return taskProtoList
 }
