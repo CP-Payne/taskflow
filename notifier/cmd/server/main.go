7b3429b68c7f888d786a26451bad8fcddedf435d
CP Payne <charlethical1@gmail.com>
2025-04-10 17:46:31 +0200

    Implement notifier entry point

--- notifier/cmd/server/main.go
+++ notifier/cmd/server/main.go
@@ -1,25 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/CP-Payne/taskflow/notifier/internal/notification"
+	"github.com/CP-Payne/taskflow/notifier/internal/subscriber"
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
 )
 
-func subscribeAndSendNotification() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-	defer rdb.Close()
-
-	pubsub := rdb.Subscribe(ctx, "task_notification")
-	ch := pubsub.Channel()
-
-	for msg := range ch {
-		fmt.Println("Sending Notification:", msg.Payload)
-		// Call Email/SMS API
-	}
-}
+// TODO: Implement Gateway to retrieve user email by ID from User Service
+// TODO: Implement Email sender
 
 func main() {
+	logger := zap.Must(zap.NewProduction()).Sugar()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := godotenv.Load("./config/.env"); err != nil {
+		logger.Panic("Failed to load global config")
+	}
+
+	if err := godotenv.Load("./notifier/internal/config/.env"); err != nil {
+		logger.Panic("Failed to load notifer service config")
+	}
+
+	// TODO: Add to .env and load from config
+	redisAddr := os.Getenv("REDIS_NOTIFIER_ADDR")
+	// redisAddr := "localhost:6379"
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	})
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		logger.Errorw("Failed to connect to Redis", "error", err)
+	}
+	defer rdb.Close()
+	logger.Info("Connected to Redis")
+
+	notificationSender := notification.NewLogSender(logger)
+	redisSubscriber := subscriber.NewRedisSubscriber(rdb, notificationSender, logger)
+
+	go redisSubscriber.SubscribeAndProcess(ctx)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+
+	logger.Info("Shutting down notifier service...")
+	cancel()
+
+	logger.Info("Notifier service stopped.")
 }
